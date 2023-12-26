5bd1ac9685f3d1c2ec86eb0c24f56b95bb06bdc3
anujYadav <anujvns28@gmail.com>
2023-12-26 14:33:56 +0530

    all file added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center text2xl font-semibold">
+     hello Anuj! , kya hal chal your react app has started 
+    </div>
+  );
+}
+
+export default App;
